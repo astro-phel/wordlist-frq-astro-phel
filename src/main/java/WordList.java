4b82301258d58410132b6051064232260ef326be
astro-phel <katgranads@gmail.com>
2025-06-02 18:38:55 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -11,12 +11,18 @@ public class WordList
   public int numWordsOfLength(int len)
   {
     /* to be implemented in part (a) */
-    
+    int count = 0;
+    for(int i = 0; i <=myList.size(); i++)
+      if(myList.get(i).length()<=len)
+        count += len;
+    return count;
   }
 
   public void removeWordsOfLength(int len)
   {
     /* to be implemented in part (b) */
-
+    for(int i = 0; i <=myList.size();i++)
+      if(myList.get(i).length() == len)
+        myList.remove(i);
   }
 }
